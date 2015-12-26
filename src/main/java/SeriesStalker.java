a47d43eb3969919cb8e6fa140c84e42a1cc19e1c
Patrick-Willis <patrick_m_willis@hotmail.com>
2015-12-26 19:22:03 +0000

    Update so Spark can get the Heroku port

--- src/main/java/SeriesStalker.java
+++ src/main/java/SeriesStalker.java
@@ -1,10 +1,15 @@
 import com.google.gson.Gson;
 
 import static spark.Spark.get;
+import static spark.SparkBase.port;
 
 public class SeriesStalker {
 
+    public static final int HEROKU_DEFAULT_PORT = 4567;
+    public static final String PORT = "PORT";
+
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         Gson gson = new Gson();
         get("/",
             (request, response) -> {
@@ -17,5 +22,13 @@ public class SeriesStalker {
 
     public static String login(String user) {
         return "Hello " + user + "!";
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get(PORT) != null) {
+            return Integer.parseInt(processBuilder.environment().get(PORT));
+        }
+        return HEROKU_DEFAULT_PORT;
     }
 }
